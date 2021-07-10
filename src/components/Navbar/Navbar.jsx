2dbccc6b5283266a77ad123daa5ed87a6696f136
Sambit_Sankalp <sambit.sankalp.official@gmail.com>
2021-07-10 18:46:47 +0530

    nav change

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,37 @@
+import {
+    Nav,
+    NavLink,
+    Bars,
+    NavMenu,
+    NavBtn,
+    NavBtnLink,
+  } from './styledElements';
+    
+  const Navbar = () => {
+    return (
+      <>
+        <Nav>
+          <Bars />
+    
+          <NavMenu>
+            <NavLink to='/' activeStyle>
+              Home
+            </NavLink>
+            <NavLink to='/articles' activeStyle>
+              Articles
+            </NavLink>
+            <NavLink to='/about' activeStyle>
+              About
+            </NavLink>
+            {/* Second Nav */}
+            {/* <NavBtnLink to='/sign-in'>Sign In</NavBtnLink> */}
+          </NavMenu>
+          <NavBtn>
+            <NavBtnLink to='/signin'>Sign In</NavBtnLink>
+          </NavBtn>
+        </Nav>
+      </>
+    );
+  };
+    
+  export default Navbar;
